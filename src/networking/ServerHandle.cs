9eb6d994296561662d9c431de35c737eb4579fff
achter <90976703+achterx@users.noreply.github.com>
2026-01-16 23:47:06 +0300

    Update ServerHandle.cs

--- src/networking/ServerHandle.cs
+++ src/networking/ServerHandle.cs
@@ -4519,10 +4519,10 @@ namespace H3MP.Networking
             {
                 if (Mod.currentTNHInstance.manager != null && !Mod.currentTNHInstance.holdOngoing)
                 {
-                    GM.CurrentMovementManager.TeleportToPoint(Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex].SpawnPoint_SystemNode.position, true);
-                    Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex].m_systemNode.m_hasActivated = true;
-                    Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex].m_systemNode.m_hasInitiatedHold = true;
-                    Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex].BeginHoldChallenge();
+Vector3 spawnPos = TNH_HoldPointPatch.SafeGetSystemNodeSpawnPoint(Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex]);
+GM.CurrentMovementManager.TeleportToPoint(spawnPos, true);
+TNH_HoldPointPatch.SafeSetSystemNodeFlags(Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex], true, true);
+Mod.currentTNHInstance.manager.HoldPoints[Mod.currentTNHInstance.curHoldIndex].BeginHoldChallenge();
                 }
             }
             else
